11f2d441e8f03e2dbae4bf8f6fbd61e8d3e6e3da
kawau-so <s.suguru4869@gmail.com>
2024-09-20 22:46:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.querySelector("#btn");
 btn.addEventListener("click", notification);
 
 let i = 0;
-while (i < 5) {
+while (i < 100) {
   function notification() {
   switch (Notification.permission) {
     case "default":
@@ -31,7 +31,7 @@ do {
       alert("通知が拒否されています");
       break;(`iの値は ${i}`);
   i++;
-} while (i < 5);
+} while (i < 100);
 
 /* while do...while共通
 iの値は 0
