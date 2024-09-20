d8087e5b4e3cffff026fc52fc0daa0cf15bbb4e0
kawau-so <s.suguru4869@gmail.com>
2024-09-20 22:44:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", notification);
 
-function notification() {
+let i = 0;
+while (i < 5) {
+  function notification() {
   switch (Notification.permission) {
     case "default":
       Notification.requestPermission();
@@ -11,6 +13,30 @@ function notification() {
       break;
     case "denied":
       alert("通知が拒否されています");
-      break;
-  }
+      break;(`iの値は ${i}`);
+  i++;
 }
+
+let i = 0;
+do {
+  function notification() {
+  switch (Notification.permission) {
+    case "default":
+      Notification.requestPermission();
+      break;
+    case "granted":
+      new Notification("すぐるんちゃっとのメッセージ");
+      break;
+    case "denied":
+      alert("通知が拒否されています");
+      break;(`iの値は ${i}`);
+  i++;
+} while (i < 5);
+
+/* while do...while共通
+iの値は 0
+iの値は 1
+iの値は 2
+iの値は 3
+iの値は 4
+*/
